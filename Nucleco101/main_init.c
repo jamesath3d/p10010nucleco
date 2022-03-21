220f939db13be3680e0a46b0720269f232e15bcc
Your Name <you@example.com>
2022-03-21 11:07:36 -0400

    2022_0321_1107am_35

--- Nucleco101/main_init.c
+++ Nucleco101/main_init.c
@@ -1,8 +1,10 @@
 
 #include "main.h"
 
+void gpio_init(void) {
+} // gpio_init
 void main_init(void){
     _WDT_disable; // _wdt_init.h
-    ClkInit();    // _clk_init_16mhz Clk16mhz // main.h
+    ClkInit( & gpio_init );    // _clk_init_16mhz Clk16mhz // main.h
 
 } // main_init
