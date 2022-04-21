2961d75d1cf7391d175a117d08a58006cf29080c
Your Name <you@example.com>
2022-04-21 11:03:40 -0400

    2022_0421_1103am_40

--- Nucleco101/main.c
+++ Nucleco101/main.c
@@ -1,86 +1,36 @@
 #include "main.h"
 
-/*
-uint16_t _sCNT = 0 ;
-void main_loop01(void){
-    _sCNT ++ ;
-    if ( _sCNT & 0x1 ) {
-        _Set1_(vsync3);
-    } else {
-        _Set0_(vsync3);
-    }
-    if ( _sCNT & 0x2 ) {
-        _Set1_(vsync4);
-    } else {
-        _Set0_(vsync4);
-    }
-} // main_loop01
-
-inline void main_loop02(void){
-    _Set1_(vsync4);
-
-    _Set1_(vsync3);
-    _Set0_(vsync3);
-    //_Set1_(vsync3);
-    //_Set0_(vsync3);
-
-    _Set0_(vsync4);
-
-    _Set1_(vsync3);
-    _Set0_(vsync3);
-    //_Set1_(vsync3);
-    //_Set0_(vsync3);
-} // main_loop02
-*/
-
-inline void main_loop03(void){
-    _Set1_(vsync4);
-    _Set0_(vsync3);
-    __nop();
-    __nop();
-    _Set0_(vsync4);
-    _Set1_(vsync3);
-    //_Set1_(vsync3);
-    //_Set0_(vsync3);
-} // main_loop03
-
-uint8_t rc01 ;
-uint8_t rc02 ;
-
-int mainX1(void) {
+int main(void) {
     main_init(); // _clk_init_16mhz
 
-
-    if ( 1 ) {
-        _Set0_(vsync3);
-        _Set1_(vsync4);
-        _Set0_(vsync5);
-        _Set1_(vsync6);
-        rc01 = P1OUT;
-        _Set1_(vsync3);
-        _Set0_(vsync4);
-        _Set1_(vsync5);
-        _Set0_(vsync6);
-        rc02 = P1OUT;
+    while(0){
+        _WDT_wait_interrupt_LPM4 ;  // Enter LPM3
+        __nop();                    // For debugger
     }
+
+    //_gpio_mclk_p13  ;
+    //_gpio_smclk_p17 ;
+    //_gpio_aclk_p22  ;
+
+    
+    P1DIR |= BIT1 | BIT2;                     // P1.1 and P1.2 output
+
+    //P1SEL1 |= BIT1 | BIT2;                    // P1.1 and P1.2 options select
+    P1SEL1 |= BIT1 ;                    // P1.1 
+    
+    TA0CCTL1 = OUTMOD_7;                      // CCR1 reset/set
+    TA0CCTL2 = OUTMOD_2;                      // CCR2 reset/set
+    TA0CCR0 = 12-1;                         // PWM Period
+    TA0CCR1 = 6;                            // CCR1 PWM duty cycle
+    TA0CCR2 = 6;                            // CCR2 PWM duty cycle
+
+    TA0CTL = TASSEL__SMCLK | MC__UP | TACLR;  // SMCLK, up mode, clear TAR
+
 
     while(1){
-        // main_loop01();
-        //main_loop02();
-        //main_loop03();
-        //_Set0_(vsync3); _Set1_(vsync3);
-
-        //Delay_1000ms();
-        if (1){
-            P1OUT=rc01;
-            __delay_cycles(1);
-            P1OUT=rc02;
-        }
+        _WDT_wait_interrupt_LPM4 ;  // Enter LPM3
+        __nop();                    // For debugger
     }
-} // mainX1
-int main(void) {
-        P1DIR |= BIT3 ; P1SEL1 |= BIT3 ; P1SEL0 &= ~BIT3 ; // dir->1, selx->10 : link mclk  to p1.3, for oscilloscope debuging
-        P1DIR |= BIT7 ; P1SEL1 |= BIT7 ; P1SEL0 &= ~BIT7 ; // dir->1, selx->10 : link smclk to p1.7, for oscilloscope debuging
-        while(1){
-        }
 } // main
+
+
